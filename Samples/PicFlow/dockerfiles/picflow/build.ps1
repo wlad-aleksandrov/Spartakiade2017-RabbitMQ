e8d2999876383621002fdfdc6606ec1ab83b5b62
Frank Pommerening <frank@pommerening-online.de>
2017-01-31 21:53:55 +0100

    build script

--- Samples/PicFlow/dockerfiles/picflow/build.ps1
+++ Samples/PicFlow/dockerfiles/picflow/build.ps1
@@ -12,15 +12,15 @@ $projects = @{
     'ExternalApp' = "picflow-extapp";
 }
 
-$projects.Keys |
+$projects.GetEnumerator() |
      ForEach-Object {
-        Set-Location ('"{0}\src\{1}"' -f $projectPath, $_) 
-        & dotnet 'publish' ('-c Release -o {0}\{1}'-f $tempPath, $projects.Item($_))
+        Set-Location ('"{0}\src\{1}"' -f $projectPath, $_.Key) 
+        & dotnet 'publish' ('-c Release -o {0}\{1}'-f $tempPath, $_.Value)
         Set-Location $tempPath
-        & $zip a ('{0}.7z' -f  $projects.Item($_))  (' {0}\{1}' -f $tempPath, $projects.Item($_))
+        & $zip a ('{0}.7z' -f  $_.Value)  (' {0}\{1}' -f $tempPath, $_.Value)
         Set-Location ('"{0}\dockerfiles\{1}"' -f $projectPath, $_) 
-        Copy-Item (' {0}\{1}.7z' -f $tempPath, $projects.Item($_)) 
-        & docker 'build' ('-t fpommerening/devopenspace2016:{0} . ' -f $projects.Item($_))
+        Copy-Item (' {0}\{1}.7z' -f $tempPath, $_.Value) 
+        & docker 'build' ('-t fpommerening/spartakiade2017-rabbitmq:{0} . ' -f $_.Value)
     }
 
 
