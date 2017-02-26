f2ef116b57c0f28231d281d578fef1b38e853adf
Frank Pommerening <frank@pommerening-online.de>
2017-02-26 19:09:58 +0100

    Plugin Scheduler

---
+++ Samples/PluginScheduler/src/PluginScheduler/FutureMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FP.Spartakiade2017.MsRmq.PluginScheduler
+{
+    public class FutureMessage
+    {
+        public FutureMessage()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public string Content { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
