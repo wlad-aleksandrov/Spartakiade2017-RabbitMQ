a82daafc1d7e5a5156387f95298a306caf125367
Frank Pommerening <frank@pommerening-online.de>
2017-03-13 20:22:03 +0100

    Prepaire PluginScheduler Sample

--- Samples/PluginScheduler/src/PluginScheduler/Program.cs
+++ Samples/PluginScheduler/src/PluginScheduler/Program.cs
@@ -11,10 +11,7 @@ namespace FP.Spartakiade2017.MsRmq.PluginScheduler
             IBus myBus = null;
             try
             {
-                myBus = RabbitHutch.CreateBus("host=localhost", 
-                    register => register.Register<IScheduler, DelayedExchangeScheduler>() );
-                StartSubscription(myBus);
-                ScheduleMessage(myBus);
+               
 
             }
             catch (Exception ex)
@@ -30,28 +27,13 @@ namespace FP.Spartakiade2017.MsRmq.PluginScheduler
 
         private static void StartSubscription(IBus myBus)
         {
-            myBus.Subscribe<FutureMessage>("MyFutureMessageSubscription", msg => Console.WriteLine($"{DateTime.Now:T}: {msg.Content} (schedule at {msg.Timestamp:T})"));
+            
            
         }
 
         private static void ScheduleMessage(IBus myBus)
         {
-            string content = string.Empty;
-            do
-            {
-                Console.WriteLine("Enter the content or nothing to leave");
-                content = Console.ReadLine();
-
-                if (!string.IsNullOrEmpty(content))
-                {
-                    Console.WriteLine("Enter the delay in seconds");
-                    var delayInSeconds = Convert.ToInt32(Console.ReadLine());
-
-                    myBus.FuturePublish(TimeSpan.FromSeconds(delayInSeconds), new FutureMessage { Content = content });
-
-                }
-                System.Threading.Thread.Sleep(1000);
-            } while (!string.IsNullOrEmpty(content));
+            
 
         }
     }
