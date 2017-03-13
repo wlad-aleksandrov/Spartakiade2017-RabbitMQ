927f0f6a42bf7b287f95cd290d91eddb56d8acec
Frank Pommerening <frank@pommerening-online.de>
2017-03-13 20:20:23 +0100

    Prepare IoTApp Sample

--- Samples/IoT/src/Hub/Program.cs
+++ Samples/IoT/src/Hub/Program.cs
@@ -14,36 +14,6 @@ namespace FP.Spartakiade2017.MsRmq.IoTApp.Hub
 
             try
             {
-                var connection = new ConnectionConfiguration
-                {
-                    Port = 5671,
-                    UserName = "guest",
-                    Password = "guest",
-                    Product = "IoT Hub"
-                };
-
-                var hostConfiguration = new HostConfiguration();
-                hostConfiguration.Host = "localhost";
-                hostConfiguration.Port = 5671;
-                hostConfiguration.Ssl.Enabled = true;
-                hostConfiguration.Ssl.ServerName = "rabbitServer";
-                hostConfiguration.Ssl.CertPath = @"c:\Temp\keycert.p12";
-                hostConfiguration.Ssl.CertPassphrase = "strenggeheim";
-                hostConfiguration.Ssl.AcceptablePolicyErrors =
-                    SslPolicyErrors.RemoteCertificateNameMismatch |
-                    SslPolicyErrors.RemoteCertificateChainErrors;
-                connection.Hosts = new List<HostConfiguration> {hostConfiguration};
-                connection.Validate();
-                myBus = RabbitHutch.CreateBus(connection, services => { });
-                //myBus = RabbitHutch.CreateBus("host=10.0.1.72");
-
-                Console.WriteLine("IoT Hub is started");
-
-                myBus.Subscribe<MeteringValue>("IoTHub", mv =>
-                {
-                    Console.WriteLine($"New metering value from {mv.Host} - {mv.ObisCode} - {mv.Value} W");
-                });
-                Console.ReadLine();
             }
             catch (Exception e)
             {
